523bcf474119a0bf6cd6994864ae53b27f7662a8
Samal <56509582+FransoArbela@users.noreply.github.com>
2025-02-04 19:13:27 +0100

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,7 @@ const burgerBtn = document.querySelector(".burger-menu");
 const offscreenMenu = document.querySelector(".offscreen-menu");
 
 burgerBtn.addEventListener("click", ()=>{
-    burgerBtn.classList.toggle('is-active')
-    offscreenMenu.classList.toggle('active')
+    burgerBtn.classList.toggle('is-active');
+    offscreenMenu.classList.toggle('active');
+    document.body.classList.toggle("no-scroll");
 })
