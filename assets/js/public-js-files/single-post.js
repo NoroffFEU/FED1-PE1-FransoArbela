133e792ef79495738e263cc2b6e54524ef78f850
Samal <56509582+FransoArbela@users.noreply.github.com>
2025-02-23 18:31:28 +0100

    add copy link functionality and improve post author display in single post view

--- assets/js/public-js-files/single-post.js
+++ assets/js/public-js-files/single-post.js
@@ -7,50 +7,58 @@ import { showLoadingMessage, hideLoadingMessage } from "./loading.js";
 const params = new URLSearchParams(window.location.search);
 const postId = params.get("id");
 
-
 // getting the current username based on if admin is logged in or not.
 const username = getUsername();
 
 document.addEventListener("DOMContentLoaded", async () => {
+  // in case the url id is empty
+  if (postId === null) {
+    alert("Please provide a valid post ID.");
+    window.location.href = "/post/blog-feed.html";
+  } else {
+    showLoadingMessage();
 
-  
-    // in case the url id is empty
-    if (postId === null) {
-      alert("Please provide a valid post ID.");
-      window.location.href = "/post/blog-feed.html";
-    } else {
+    try {
+      const response = await fetch(
+        `https://v2.api.noroff.dev/blog/posts/${username}/${postId}`,
+        {
+          method: "GET",
+          headers: {
+            accept: "application/json",
+          },
+        }
+      );
+      // case of the response is ok
+      if (response.ok) {
+        const blogPosts = await response.json();
+        const fullBlog = document.querySelector(".single-post");
+        const pageUrl = window.location.href;
+        const urlCopy = () => {
+          navigator.clipboard.writeText(pageUrl);
+          alert("Link copied to clipboard");
+        }
+        
 
-      showLoadingMessage();
-
-      try {
-        const response = await fetch(
-          `https://v2.api.noroff.dev/blog/posts/${username}/${postId}`,
-          {
-            method: "GET",
-            headers: {
-              accept: "application/json",
-            },
-          }
-        );
-        // case of the response is ok
-        if (response.ok) {
-          const blogPosts = await response.json();
-          const fullBlog = document.querySelector(".single-post");
-    
-          const post = blogPosts.data;
-          // Generate post
-          const postElement = document.createElement("div");
-          postElement.className = "full-blog-post";
-          postElement.innerHTML = `
+        const post = blogPosts.data;
+        // Generate post
+        const postElement = document.createElement("div");
+        postElement.className = "full-blog-post";
+        postElement.innerHTML = `
           <h1 id="blog-feed-title">${post.title}</h1>
-          <h6 id="blog-feed-author">Author: ${post.author.name}</h6>
-    
-          <img src="${post.media?.url || ""}" alt="${post.media?.alt || "Image"}" />
+          <div class="author-and-copy-link">
+            <h6 id="blog-feed-author">Author: ${post.author.name}</h6>
+            <button data-author="" id="copy-link">
+             <img id="copy-link-img" src="../assets/images/copy-link.png" alt="copy-link">
+            </button>
+          </div>
+          <img src="${post.media?.url || ""}" alt="${
+          post.media?.alt || "Image"
+        }" />
     
           <p id="blog-feed-body">${post.body}</p>
     
           <div class="post-details">
-          <p id="blog-feed-tags">Tags: ${post.tags.join(", ")}</p>
+           <p id="blog-feed-tags">Tags: ${post.tags.join(", ")}</p>
           <div id="blog-feed-dates">
             <p id="date">Published: ${new Date(
               post.created
@@ -61,19 +69,18 @@ document.addEventListener("DOMContentLoaded", async () => {
           </div>
           </div>
         `;
-          fullBlog.appendChild(postElement);
-        } else {
-          console.error(
-            "Failed to fetch blog posts:",
-            response.status,
-            await response.text()
-          );
-        }
-        hideLoadingMessage();
-      } catch (error) {
-        console.error("Error fetching blog posts:", error);
+        postElement.querySelector("#copy-link").addEventListener("click", urlCopy);
+        fullBlog.appendChild(postElement);
+      } else {
+        console.error(
+          "Failed to fetch blog posts:",
+          response.status,
+          await response.text()
+        );
       }
-
+      hideLoadingMessage();
+    } catch (error) {
+      console.error("Error fetching blog posts:", error);
     }
- 
+  }
 });
